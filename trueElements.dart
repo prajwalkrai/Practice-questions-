c44bcca3462ad57f83efe80c332bb1b6d4d16921
Prajwal Kumar Rai <121857731+prajwalkrai@users.noreply.github.com>
2023-02-27 22:00:51 +0530

    Create trueElements.dart

---
+++ trueElements.dart
@@ -0,0 +1,7 @@
+// checking how many times a number exists
+// printing true if condition on list satisfies
+
+void main(List<String> args) {
+  List<int> list = [1, 2, 43, 34, 6, 7, 44, 24, 4, 3, 4];
+  list.forEach((element) => print(element > 10));
+}
